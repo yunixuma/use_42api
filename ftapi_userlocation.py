1e023bb79f7acd8a3aceefa5e16aff1d4013d9e1
Yoshihiro Kosaka <ykosaka@student.42tokyo.jp>
2025-01-18 17:32:06 +0900

    add userlocation

---
+++ ftapi_userlocation.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+
+import sys, os, json
+import urllib3, requests
+import my_common as my
+DEBUG = True
+ftapi = my.import_module("ftapi_common", DEBUG)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+data_dir = BASE_DIR + "/data/userlocation"
+my.mkdir(data_dir, DEBUG)
+
+my.debug_print(ftapi.ftapi_token.token, DEBUG, my.COLOR["DEBUG"])
+
+if len(sys.argv) < 2:
+    login = "ylinux"
+else:
+    login = sys.argv[1]
+datetime = my.get_datetime()
+filepath = login + "_location_" + datetime + ".json"
+filepath = data_dir + "/" + filepath
+my.debug_print(filepath, DEBUG, my.COLOR["DEBUG"])
+
+header = {
+    'Authorization' : 'Bearer ' + ftapi.ftapi_token.token
+}
+    # 'Authorization' : 'Bearer ' + ftapi_common.ftapi_token.token
+    # 'access_token': ftapi_common.ftapi_token.token
+my.debug_print(str(header), DEBUG, my.COLOR["DEBUG"])
+json_data_joined = []
+for idx_page in range(1, 200):
+    uri = f"/v2/users/{login}/locations?page[number]={idx_page}&page[size]=100"
+    my.debug_print(uri, DEBUG, my.COLOR["DEBUG"])
+    try:
+        json_data = ftapi.get_method(uri, header)
+        my.debug_print(str(json_data))
+    except:
+        my.debug_print("Exiting on failure", DEBUG, my.COLOR["FAILURE"])
+        exit(1)
+    if json_data == None or len(json_data) == 0:
+        break
+    json_data_joined += json_data
+try:
+    my.save_json(json_data_joined, filepath)
+except:
+    my.debug_print("Exiting on failure", DEBUG, my.COLOR["FAILURE"])
+    sys.exit(1)
